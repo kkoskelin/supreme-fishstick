db9d55ab7b695fa4c8a32e0665307a31c38cbc73
Kris Koskelin <kkoskelin@flexion.us>
2024-07-22 20:29:00 -0500

    Task: re-add 2024 as default search year

--- src/presenter/actions/displaySwimmerSearchResponsive.ts
+++ src/presenter/actions/displaySwimmerSearchResponsive.ts
@@ -2,6 +2,6 @@ import { Context } from '../presenter';
 
 export const displaySwimmerSearchResponsive = (context: Context) => {
   // load most recently-applied filters into the form.
-  context.state.form.recordFilter = { ...context.state.recordFilter };
+  context.state.form.recordFilter = { ...context.state.recordFilter, year: '2024' };
   context.state.currentPage = 'SwimmerSearchResponsive';
 };
