1800b5499335d50891ae8b6b14081d0da2df2fd9
Kris Koskelin <kkoskelin@flexion.us>
2024-07-22 20:20:34 -0500

    Task: remove Andrew from search terms

--- src/presenter/actions/displaySwimmerSearchResponsive.ts
+++ src/presenter/actions/displaySwimmerSearchResponsive.ts
@@ -2,6 +2,6 @@ import { Context } from '../presenter';
 
 export const displaySwimmerSearchResponsive = (context: Context) => {
   // load most recently-applied filters into the form.
-  context.state.form.recordFilter = { ...context.state.recordFilter, swimmerName: 'Koskelin, Andrew' };
+  context.state.form.recordFilter = { ...context.state.recordFilter };
   context.state.currentPage = 'SwimmerSearchResponsive';
 };
